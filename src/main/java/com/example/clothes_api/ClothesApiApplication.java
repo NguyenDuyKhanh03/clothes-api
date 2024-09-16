b878efb270251b78c556e7d92defdb180549f6af
duykhanh <duykhanhz112233@gmail.com>
2024-09-16 08:19:03 +0700

    Initial commit

---
+++ src/main/java/com/example/clothes_api/ClothesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clothes_api/ClothesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
